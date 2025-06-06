dff56b5faea166b0c370c00617ee2bbdd664f3b4
shashank shukla <shashank7016204503@gmail.com>
2025-06-06 14:30:23 +0530

    naviagtion and icons setup

--- App.tsx
+++ App.tsx
@@ -1,15 +1,55 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import AuthScreen from './src/screens/Auth/Auth';
+
+export type WithoutLoginStackList = {
+  Auth: undefined;
+};
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
+  const WithoutLoginStack = createNativeStackNavigator<WithoutLoginStackList>();
+  const handleLoginSuccess = () => {
+    setIsLogin(true);
+  };
+  useEffect(() => {
+    const checkFirstLaunch = async () => {
+      try {
+        const value = await AsyncStorage.getItem('user_details');
+        if (value === null) {
+          setIsLogin(false);
+        } else {
+          setIsLogin(JSON.parse(value));
+        }
+      } catch (error) {
+        console.error('Error checking first launch', error);
+      }
+    };
+    checkFirstLaunch();
+  }, []);
   useEffect(() => {
     SplashScreen.hide();
     console.log('shukla');
   }, []);
   return (
     <View style={Style.container}>
-      <Text>THIS IS DEVELOP BRANCH </Text>
+      {isLogin === false || isLogin === undefined ? (
+        <WithoutLoginStack.Navigator>
+          <WithoutLoginStack.Screen name="Auth" options={{headerShown: false}}>
+            {props => (
+              <AuthScreen {...props} onLoginSuccess={handleLoginSuccess} />
+            )}
+          </WithoutLoginStack.Screen>
+        </WithoutLoginStack.Navigator>
+      ) : (
+        <View>
+          <Text>THIS IS WITH LOGIN SCREEN</Text>
+        </View>
+      )}
     </View>
   );
 };
--- index.js
+++ index.js
@@ -5,5 +5,14 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {NavigationContainer} from '@react-navigation/native';
 
-AppRegistry.registerComponent(appName, () => App);
+const Main = () => {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Main);
